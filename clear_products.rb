b37331f13e13cec93f270db06214f64872909e2d
Jimmy@cp.ubu <jimmy@coolpur.com>
2010-11-02 18:35:10 +0800

    clear

--- clear_products.rb
+++ clear_products.rb
@@ -1,7 +1,13 @@
 cnt = 0
 Product.active.price_between(0,20).each do |p|
-  p.available_on = nil
-  p.save!
-  cnt += 1
-  puts cnt
+  pp = p.property('上市日期')
+  if !pp || pp < '2008'
+    p.available_on = nil
+    p.save!
+    cnt += 1
+    puts cnt
+  else
+    print '.'
+  end
+
 end
